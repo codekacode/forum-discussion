03124bcebd35c08b4d91a4711ec8fa7f2e246bad
Codekacode <karem.quiroz.m@gmail.com>
2021-01-10 11:01:02 -0500

    Create user with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :channels
+  # resources :discussions do
+  #   resources :replies
+  # end
+  
+  # devise_for :users, controller: {registrations: 'registrations'}
+  # root 'discussions#index'
 end
